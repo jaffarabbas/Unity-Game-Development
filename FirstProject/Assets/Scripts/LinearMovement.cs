ef3c8550a7045cf68be1bd1a9856a74ebe1a6e71
Jaffar Abbas <gamaportal8@gmail.com>
2023-02-13 00:01:34 +0500

    2-13-2023

--- FirstProject/Assets/Scripts/LinearMovement.cs
+++ FirstProject/Assets/Scripts/LinearMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LinearMovement : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float upSpeed = 20.0f;
+    public float reset = 0.0f;
+    public float v = -10.97f;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,32 @@ public class LinearMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    void resetGame()
+    {
+        transform.position = Vector3.right * reset;
+        transform.position += Vector3.right * v;
+    }
+    void go()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+    void goUpAndDown()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * upSpeed * Time.deltaTime; 
+            transform.position += Vector3.up * reset;
+        }
+    }
+    // Update is called once per frame
     void Update()
     {
-        
+        go();
+        if (transform.position.x > 10.0)
+        {
+            resetGame();
+            go();
+        }
+        goUpAndDown();
     }
 }
