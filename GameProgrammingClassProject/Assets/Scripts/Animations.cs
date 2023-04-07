022c5a9b10a9626b7e2be0c39219e492e3f151f5
Jaffar Abbas <gamaportal8@gmail.com>
2023-04-07 08:27:10 +0500

    4-7-2023

---
+++ GameProgrammingClassProject/Assets/Scripts/Animations.cs
@@ -0,0 +1,180 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed = 5f;
+
+    private Animator animator;
+
+    private float xAxis;
+    private float yAxis;
+    private Rigidbody2D rb2d;
+    private bool isJumpPressed;
+    private float jumpForce = 850;
+    private int groundMask;
+    private bool isGrounded;
+    private string currentAnimaton;
+    private bool isAttackPressed;
+    private bool isAttacking;
+
+    [SerializeField]
+    private float attackDelay = 1f;
+
+    //Animation States
+    const string PLAYER_IDLE = "idle";
+    const string PLAYER_RUN = "walking";    
+    const string PLAYER_JUMP = "jump";
+    const string PLAYER_ATTACK = "Player_attack";
+    const string PLAYER_AIR_ATTACK = "Player_air_attack";
+
+    //=====================================================
+    // Start is called before the first frame update
+    //=====================================================
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        groundMask = 1 << LayerMask.NameToLayer("Ground");
+    }
+
+    //=====================================================
+    // Update is called once per frame
+    //=====================================================
+    void Update()
+    {
+        //Checking for inputs
+        xAxis = Input.GetAxisRaw("Horizontal");
+
+        //space jump key pressed?
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumpPressed = true;
+        }
+
+        //space Atatck key pressed?
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            Debug.Log("Attack");
+            isAttackPressed = true;
+        }
+    }
+
+    //=====================================================
+    // Physics based time step loop
+    //=====================================================
+    private void FixedUpdate()
+    {
+        //check if player is on the ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, groundMask);
+
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        //------------------------------------------
+
+        //Check update movement based on input
+        Vector2 vel = new Vector2(0, rb2d.velocity.y);
+
+
+
+        if (xAxis < 0)
+        {
+            Debug.Log("Left");
+            vel.x = -walkSpeed;
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if (xAxis > 0)
+        {
+            Debug.Log("Right");
+            vel.x = walkSpeed;
+            transform.localScale = new Vector2(1, 1);
+            ChangeAnimationState(PLAYER_RUN);
+        }
+        else
+        {
+            vel.x = 0;
+        }
+
+        if (xAxis != 0)
+        {
+            ChangeAnimationState(PLAYER_RUN);
+        }
+        else
+        {
+            ChangeAnimationState(PLAYER_IDLE);
+        }
+
+        if (isGrounded && !isAttacking)
+        {
+            if (xAxis != 0)
+            {
+                ChangeAnimationState(PLAYER_RUN);
+            }
+            else
+            {
+                ChangeAnimationState(PLAYER_IDLE);
+            }
+        }
+
+        //------------------------------------------
+
+        //Check if trying to jump 
+        if (isJumpPressed && !isGrounded)
+        {
+            Debug.Log("Jump");
+            rb2d.AddForce(new Vector2(0, jumpForce));
+            isJumpPressed = false;
+            ChangeAnimationState(PLAYER_JUMP);
+        }
+
+        //assign the new velocity to the rigidbody
+        rb2d.velocity = vel;
+
+
+        //attack
+        if (isAttackPressed)
+        {
+            isAttackPressed = false;
+
+            if (!isAttacking)
+            {
+                isAttacking = true;
+
+                if (isGrounded)
+                {
+                    ChangeAnimationState(PLAYER_ATTACK);
+                }
+                else
+                {
+                    ChangeAnimationState(PLAYER_AIR_ATTACK);
+                }
+                Invoke("AttackComplete", attackDelay);
+            }
+        }
+    }
+
+    void AttackComplete()
+    {
+        isAttacking = false;
+    }
+
+    //=====================================================
+    // mini animation manager
+    //=====================================================
+    void ChangeAnimationState(string newAnimation)
+    {
+        if (currentAnimaton == newAnimation) return;
+        animator.Play(newAnimation);
+        currentAnimaton = newAnimation;
+    }
+
+}
--- GameProgrammingClassProject/Assets/Scripts/MovingAnimation.cs
+++ GameProgrammingClassProject/Assets/Scripts/MovingAnimation.cs
@@ -6,7 +6,8 @@ public class MovingAnimation : MonoBehaviour
 {
     public string objectTag = "Player"; // The tag of the game object to move
     public float moveSpeed = 5.0f; // The speed at which to move the game object horizontally
-
+    public float jumpForce = 2.0f; // The force with which to make the object jump
+    public bool isGrounded = true;
     void Start()
     {
     }
@@ -16,13 +17,30 @@ public class MovingAnimation : MonoBehaviour
         if (this.gameObject.tag == objectTag)
         {
             float horizontalInput = Input.GetAxisRaw("Horizontal");
-
+            float verticalInput = Input.GetAxisRaw("Jump");
+            if (isGrounded && verticalInput > 0)
+            {
+                Debug.Log(isGrounded);
+                Debug.Log("Jump");
+                // Make the object jump
+                this.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+                isGrounded = false;
+                Debug.Log(isGrounded);
+            }
             float horizontalMovement = horizontalInput * moveSpeed * Time.deltaTime;
-
             this.gameObject.transform.Translate(horizontalMovement, 0, 0);
         }
     }
 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            // Set the isGrounded flag to true when the object collides with the ground
+            isGrounded = true;
+        }
+    }
 
     //Set animator parameters anim.SetBool ("run", horizontal Input = 0);
     // private Animator animator;
